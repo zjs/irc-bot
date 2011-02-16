5bc8b243a3a85bf55c7a963a495da3c214a491a2
Zach Shepherd <git@zjs.name>
2011-02-15 22:23:58 -0500

    Add leading underscores to private variables and methods to conform to PEP 8

--- ExampleCommandHandler.py
+++ ExampleCommandHandler.py
@@ -21,10 +21,10 @@ class ExampleCommandHandler(CommandHandler):
                                    the regular expression evaluates to true on
                                    any message sent directly to the bot.
         """
-        self.callback = callback
-        self.direct_prefix = nickname + ":"
-        self.globalCommandDictionary = globalCommandDictionary
-        self.directCommandDictionary = directCommandDictionary
+        self._callback = callback
+        self._direct_prefix = nickname + ":"
+        self._globalCommandDictionary = globalCommandDictionary
+        self._directCommandDictionary = directCommandDictionary
 
     def handle(self, username, channel, message):
         """This method will be called on each message received.
@@ -34,32 +34,32 @@ class ExampleCommandHandler(CommandHandler):
         channel -- The channel that the message was sent in.
         message -- The message which was received.
         """
-        if message.startswith(self.direct_prefix):
-            message = message[len(self.direct_prefix):].lstrip()
+        if message.startswith(self._direct_prefix):
+            message = message[len(self._direct_prefix):].lstrip()
 
-            self.handleDictionary(self.directCommandDictionary, username, channel, message)
+            self._handleDictionary(self._directCommandDictionary, username, channel, message)
         else:
-            self.handleDictionary(self.globalCommandDictionary, username, channel, message)
+            self._handleDictionary(self._globalCommandDictionary, username, channel, message)
 
-    def handleDictionary(self, dictionary, username, channel, message):
+    def _handleDictionary(self, dictionary, username, channel, message):
         for pattern, handler in dictionary.items():
             if re.match(pattern, message):
-                self.processActions(handler(username, channel, message))        
+                self._processActions(handler(username, channel, message))        
 
-    def processActions(self, actions):
+    def _processActions(self, actions):
         if isinstance(actions, list):
             for action in actions:
-                self.processAction(action)
+                self._processAction(action)
         else:
-            self.processAction(actions)
+            self._processAction(actions)
 
-    def processAction(self, action):
+    def _processAction(self, action):
         target = action.get("target")
         if isinstance(action.get("message"), list):
             for message in action.get("message"):
-                self.callback(action.get("target"), message)
+                self._callback(action.get("target"), message)
         else:
-            self.callback(action.get("target"), action.get("message"))
+            self._callback(action.get("target"), action.get("message"))
 
 
 
