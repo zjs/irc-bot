d1229d4f048ff70a3455e73e557e477e28e40011
Zach Shepherd <github.com@zjs.name>
2011-02-16 07:49:22 -0500

    An example of adding a 'next' command which increments a persistent counter, sends the new counter value to the channel, and alerts the user that they've incremented the counter.

--- ExampleCommandHandlerFactory.py
+++ ExampleCommandHandlerFactory.py
@@ -5,13 +5,26 @@ class ExampleCommandHandlerFactory(CommandHandlerFactory):
     """A factory for instances of ExampleCommandHandler"""
     def __init__(self, nickname):
 	self.commands = Commands()
+	self.counter = Counter()
         self.nickname = nickname
 
     def create(self, callback):
-	return ExampleCommandHandler(callback, self.nickname, {"help$": self.commands.help},  {"help": self.commands.help},)
+	return ExampleCommandHandler(callback, self.nickname, {"help$": self.commands.help, "next$": self.counter.incrementAndPrint},  {"help": self.commands.help},)
 
 class Commands:
     """A container class for commands used by ExampleCommandHandlerFactory"""
+
     def help(self, user, channel, input):
         message = ["Available commands:","  help - display this message"]
         return {"target": user, "message": message}
+
+
+class Counter:
+    def __init__(self):
+        self.counter = 0
+
+    def incrementAndPrint(self, user, channel, input):
+        self.counter = self.counter + 1
+        message = "Count is now %d" % self.counter
+        return [{"target": channel, "message": message}, {"target": user, "message": "You've incremented the counter"}]
+
